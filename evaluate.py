ddbfb084e5c4be8a03f0bb6780ed582c0dfe7f5a
Ades551 <ades6800@gmail.com>
2024-04-14 00:23:24 +0200

    update

--- evaluate.py
+++ evaluate.py
@@ -5,7 +5,7 @@ from multiprocessing import Pool, cpu_count
 import pandas as pd
 from tqdm import tqdm
 
-NUM_EXAMPLES = 5
+NUM_EXAMPLES = 50
 
 # Set your OpenAI API key
 client = OpenAI(api_key=environ["OPENAI_API_KEY"])
@@ -25,8 +25,8 @@ def evaluate(data: tuple[str, str]) -> tuple[str, str, str | None]:
     text, error = data
 
     prompt = (
-        f"Jsi poslušný asistent\n"
-        f"Oprav chyby v textu, pokud tam jsou: `{error}`"
+        f"Jsi poslušný asistent.\n"
+        f"Oprav chyby v textu, pokud tam jsou: `{error or text}`"
     )
 
     response = client.chat.completions.create(
@@ -51,7 +51,7 @@ evaluation_df = pd.DataFrame(columns=['text', 'error', 'correction'])
 with Pool(processes=cpu_count()) as p, tqdm(total=NUM_EXAMPLES, desc="Sending API calls to GPT.") as pbar:
     for result in p.imap_unordered(evaluate, data_generator()):
         if result[2] is not None:
-            evaluation_df = evaluation_df.append({'text': result[0], 'error': result[1], 'correction': result[2]}, ignore_index=True)
+            evaluation_df = evaluation_df._append({'text': result[0], 'error': result[1], 'correction': result[2]}, ignore_index=True)
         pbar.update()
 
 print("Saving evaluation data...")
